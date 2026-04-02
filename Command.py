0e575778584ae53d7c524df2960f16c6cd84388f
Nathan Mantel <mantelnathan2018@gmail.com>
2026-04-02 11:49:18 +0200

    Refactor command classes and add account management

--- Command.py
+++ Command.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
-from bank import Compte
+from bank import Compte, CentralExchangeBank
 
 class Command(ABC):
     @abstractmethod
     def execute(self) -> None:
         pass
 
-class DeposerCommand(Command):
+class DeposerMoneyCommand(Command):
     def __init__(self, compte, value: float):
         self._compte = compte
         self._value = value
@@ -17,7 +17,7 @@ class DeposerCommand(Command):
     def undo(self):
         self._compte.remove_money(self._value)
 
-class RetirerCommand(Command):
+class RetirerMoneyCommand(Command):
     def __init__(self, compte, value: float):
         self._compte = compte
         self._value = value
@@ -27,3 +27,25 @@ class RetirerCommand(Command):
 
     def undo(self):
         self._compte.add_money(self._value)
+        
+class AjouterCompteCommand(Command):
+    def __init__(self, _bank: CentralExchangeBank, _compte):
+        self._bank = _bank
+        self._compte = _compte
+        
+    def execute(self):
+        self._bank.add_compte(self._compte)
+        
+    def undo(self):
+        self._bank.remove_compte(self._compte)
+        
+class RetirerCompteCommand(Command):
+    def __init__(self, _bank: CentralExchangeBank, _compte):
+        self._bank = _bank
+        self._compte = _compte
+        
+    def execute(self):
+        self._bank.remove_compte(self._compte)
+        
+    def undo(self):
+        self._bank.add_compte(self._compte)
