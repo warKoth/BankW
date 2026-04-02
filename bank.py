64150041e5e0f4ecaaadcfa958b54659329e99af
Nathan Mantel <mantelnathan2018@gmail.com>
2026-04-02 18:19:59 +0200

    fix Observer pattern

--- bank.py
+++ bank.py
@@ -1,48 +1,55 @@
 from abc import ABC, abstractmethod
-from Command import DeposerMoneyCommand, RetirerMoneyCommand
+from typing import List
+import Command
 import Invoker
-
 
 class Observer(ABC):
 
     @abstractmethod
-    def update(self, sujet, montant) -> None:
-        """
-        Receive update from subject.
-        """
+    def update(self, sujet, event: str, montant: int) -> None:
         pass
 
 
+class Subject(ABC):
+    def __init__(self):
+        self._observers: List[Observer] = []
+
+    def attach(self, observer: Observer):
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer):
+        self._observers.remove(observer)
+
+    def notify(self, event: str, value: int):
+        for observer in self._observers:
+            observer.update(self, event, value)  
+
 class HistoricObserver(Observer):
-    
+
     def __init__(self):
         self._transaction = []
-        
-    def update(self, sujet, montant):
-        self._transaction.append(f"{sujet}: {montant}")
+
+    def update(self, sujet, event: str, montant: int):  
+        self._transaction.append(f"{sujet.nom} - {event}: {montant}")
+
+    @property
+    def transactions(self):
+        return self._transaction
 
 
-class Compte:
-    
-    def __init__(self,nom: str,ref: str = None, _solde: int = None, utilisateur: str = None, _observer: Observer = []):
+class Compte(Subject):  
+
+    def __init__(self, nom: str, ref: str = None, _solde: int = 0, utilisateur: str = ""):
+        super().__init__()  
         self.nom = nom
         self.ref = ref if ref else self.nom
-        self._solde = _solde if _solde else 0
-        self.utilisateur = utilisateur if utilisateur else ""
-        self._observer = []
+        self._solde = _solde  
+        self.utilisateur = utilisateur
 
-    def ajouter_observer(self, observer):
-        self._observer.append(observer)
-        
-    def _notify(self, sujet, montant):
-        for observer in self._observer:
-            observer.update(sujet, montant)
-
-    
     @property
     def solde(self):
         return self._solde
-    
+
     @solde.setter
     def solde(self, value):
         if value < 0:
@@ -51,14 +58,15 @@ class Compte:
 
     def add_money(self, value_to_add):
         self._solde += value_to_add
-        self._notify("dépot",value_to_add)
+        self.notify("dépôt", value_to_add)
 
     def remove_money(self, value_to_remove):
-        if self._solde - value_to_remove > 0:
+        if self._solde - value_to_remove >= 0:  
             self._solde -= value_to_remove
-            self._notify("retrait", value_to_remove)
+            self.notify("retrait", value_to_remove)
 
-class CentralExchangeBank:
+
+class CentralExchangeBank(Subject):
     def __init__(self, nom: str, _identifiant: int):
         self.nom = nom
         self._identifiant = _identifiant
@@ -73,6 +81,8 @@ class CentralExchangeBank:
             raise ValueError("Le compte existe déjà dans la bank")
         else:
             self._list_compte.append(compte)
+            self.notify("Nouveaux compte", 00)
 
     def remove_compte(self, compte: Compte):
-        self._list_compte = [c for c in self._list_compte if compte != compte]
+        self._list_compte = [c for c in self._list_compte if c != compte]  
+        self.notify("Suppréssion de compte")
