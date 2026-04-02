c139667f6dc200638cd495fe4e7de635db03501e
Nathan Mantel <mantelnathan2018@gmail.com>
2026-04-02 17:46:44 +0200

    fix

--- bank.py
+++ bank.py
@@ -2,14 +2,43 @@ from abc import ABC, abstractmethod
 from Command import DeposerCommand, RetirerCommand
 import Invoker
 
+
+class Observer(ABC):
+
+    @abstractmethod
+    def update(self, sujet, montant) -> None:
+        """
+        Receive update from subject.
+        """
+        pass
+
+
+class HistoricObserver(Observer):
+    
+    def __init__(self):
+        self._transaction = []
+        
+    def update(self, sujet, montant):
+        self._transaction.append(f"{sujet}: {montant}")
+
+
 class Compte:
     
-    def __init__(self,nom: str,ref: str = None, _solde: int = None, utilisateur: str = None):
+    def __init__(self,nom: str,ref: str = None, _solde: int = None, utilisateur: str = None, _observer: Observer = []):
         self.nom = nom
-        self.ref = ref if ref else ref = self.nom
-        self._solde = _solde if _solde else _solde = 0
-        self.utilisateur = utilisateur if utilisateur else utilisateur = ""
+        self.ref = ref if ref else self.nom
+        self._solde = _solde if _solde else 0
+        self.utilisateur = utilisateur if utilisateur else ""
+        self._observer = []
 
+    def ajouter_observer(self, observer):
+        self._observer.append(observer)
+        
+    def _notify(self, sujet, montant):
+        for observer in self._observer:
+            observer.update(sujet, montant)
+
+    
     @property
     def solde(self):
         return self._solde
@@ -22,11 +51,28 @@ class Compte:
 
     def add_money(self, value_to_add):
         self._solde += value_to_add
-        # to add : notify
+        self._notify("dépot",value_to_add)
 
     def remove_money(self, value_to_remove):
         if self._solde - value_to_remove > 0:
             self._solde -= value_to_remove
-            # to add : notify
+            self._notify("retrait", value_to_remove)
 
-    
+class CentralExchangeBank:
+    def __init__(self, nom: str, _identifiant: int):
+        self.nom = nom
+        self._identifiant = _identifiant
+        self._list_compte = []
+
+    @property
+    def list_compte(self):
+        return self._list_compte
+
+    def add_compte(self, compte: Compte):
+        if compte in self._list_compte:
+            raise ValueError("Le compte existe déjà dans la bank")
+        else:
+            self._list_compte.append(compte)
+
+    def remove_compte(self, compte: Compte):
+        self._list_compte = [c for c in self._list_compte if compte != compte]
