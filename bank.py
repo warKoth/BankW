5029d270005b001d8bfca99b521c6a655a6faa95
Nathan Mantel <mantelnathan2018@gmail.com>
2026-04-02 11:07:30 +0200

    Add Observer pattern for transaction updates

--- bank.py
+++ bank.py
@@ -1,15 +1,43 @@
 from abc import ABC, abstractmethod
 from Command import DeposerCommand, RetirerCommand
 import Invoker
+ 
 
+class Observer(ABC):
+
+    @abstractmethod
+    def update(self, sujet, montant) -> None:
+        """
+        Receive update from subject.
+        """
+        pass
+
+
+class HistoricObserver(Observer):
+    
+    def __init__(self):
+        self._transaction = []
+        
+    def update(self, sujet, montant):
+        self._transaction.append(f"{sujet}: {montant}")
+        
+        
 class Compte:
     
     def __init__(self,nom: str,ref: str = None, _solde: int = None, utilisateur: str = None):
         self.nom = nom
-        self.ref = ref if ref else ref = self.nom
-        self._solde = _solde if _solde else _solde = 0
-        self.utilisateur = utilisateur if utilisateur else utilisateur = ""
-
+        self.ref = ref if ref else self.nom
+        self._solde = _solde if _solde else 0
+        self.utilisateur = utilisateur if utilisateur else ""
+        self._observer = []
+    
+    def ajouter_observer(self, observer):
+        self._observer.append(observer)
+        
+    def _notify(self, sujet, montant):
+        for observer in self._observers:
+            observer.update(sujet, montant)
+            
     @property
     def solde(self):
         return self._solde
@@ -22,11 +50,11 @@ class Compte:
 
     def add_money(self, value_to_add):
         self._solde += value_to_add
-        # to add : notify
+        self._notify("dépot",value_to_add)
 
     def remove_money(self, value_to_remove):
         if self._solde - value_to_remove > 0:
             self._solde -= value_to_remove
-            # to add : notify
+            self._notify("retrait", value_to_remove)
 
     
